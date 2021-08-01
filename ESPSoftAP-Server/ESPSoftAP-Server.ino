834a3cf0f3f1039a4119898feb0adcc7e85e8cbd
nadinCodeHat <nadinpethiyagoda4@gmail.com>
2021-08-01 10:56:17 +0530

    Configure Soft AP to NodeMCU ESP8266_01

---
+++ ESPSoftAP-Server/ESPSoftAP-Server.ino
@@ -0,0 +1,21 @@
+#include <ESP8266WiFi.h>
+
+const char *ssid = "ESPSoftAP";
+const char *password = "espsoftap";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("Configuring Soft Access Point...");
+  WiFi.mode(WIFI_AP);
+  Serial.println(WiFi.softAP(ssid, password) ? "Ready" : "Failed!");
+  
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+}
+
+void loop() {
+  Serial.printf("Stations connected = %d\n", WiFi.softAPgetStationNum());
+  delay(3000);
+}
